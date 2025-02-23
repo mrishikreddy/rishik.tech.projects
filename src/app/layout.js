a9ffb0ae247b3e1d1e6e1eb15233ebf9c004b31f
mrishikreddy <malerishikreddy@gmail.com>
2025-02-24 01:24:51 +0530

    Paradise Resorts v2

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,13 @@ import "./globals.css";
 export const metadata = {
   title: "Rishik Tech Projects",
   description: "These projects are created by Rishik Reddy",
+  keywords: "Rishik Reddy, Rishik Tech, Reddy, Male Rishik Reddy, Software Developer, Software Engineer, Machine Learning, Portfolio",
+  author: "Rishik Reddy",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
+
 
 // ✅ Correct way to define viewport separately
 export const viewport = {
